954af772b4df31f17f67ff457ea478a15509af67
Shubham Abhang <103139188+Itshubhh@users.noreply.github.com>
2026-05-27 12:51:40 +0530

    Add Firebase messaging service worker setup

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,24 @@
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
+
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+
+apiKey: "AIzaSyAlOrIXWqq1XEe1ya6UYx_csH6ckvo_x84",
+
+authDomain: "project-dashboard-ee104.firebaseapp.com",
+
+projectId: "project-dashboard-ee104",
+
+storageBucket: "project-dashboard-ee104.firebasestorage.app",
+
+messagingSenderId: "462837501926",
+
+appId: "1:462837501926:web:aed16fc192890d16115027",
+
+measurementId: "G-0FDEWTYKB2"
+
+});
+
+const messaging =
+firebase.messaging();
